1d9916982f04e33dd93bfd32e585c6c1ffa44499
srujanksofficial <161583309+srujanksofficial@users.noreply.github.com>
2025-03-05 00:55:04 +0530

    Add files via upload

--- MockApi.py
+++ MockApi.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 import random
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # List of sample stocks
 stocks = {
